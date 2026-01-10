495ccdf3f27c325b2c581235434c8d49dab1004d
Suruchi Sagar <suruchisagar24@gmail.com>
2026-01-11 02:34:07 +0530

    created signin, signup routes

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,4 +1,89 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "@repo/backend-common/config";
+import { middleware } from "./middleware";
+import {CreateUserSchema, SigninSchema, CreateRoomSchema} from "@repo/common/types";
+import { prismaClient } from "@repo/db/client";
 
 const app = express();
+app.use(express.json());
+
+
+//signup
+app.post("/signup",async (req, res)=>{
+
+    const parsedData= CreateUserSchema.safeParse(req.body);
+    if(!parsedData.success){
+        res.json({
+            "message":"Incorrect inputs"
+        })
+        return;
+    }
+
+    try{
+        const user = await prismaClient.user.create({
+        data: {
+            email:parsedData.data?.username,
+            password: parsedData.data.password,
+            name: parsedData.data.name
+        }
+    })
+    res.json({
+        userId: user.id
+    })
+
+    }
+    catch(e){
+        res.status(411).json({
+            message: "User already exists with this username"
+        })
+
+    }
+    //db call
+
+    res.json({
+        userId:"123"
+    })
+
+})
+
+app.post("/signin",(req,res)=>{
+
+    const data= SigninSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            "message":"Incorrect inputs"
+        })
+        return;
+    }
+
+    const userId=1;
+    const token=jwt.sign({
+        userId
+    }, JWT_SECRET);
+
+    res.json({
+        token
+    })
+
+})
+
+app.post("/room", middleware, (req,res)=>{
+
+    const data= CreateRoomSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            "message":"Incorrect inputs"
+        })
+        return;
+    }
+
+    //db call
+
+    res.json({
+        roomId:123
+    })
+
+})
+
 app.listen(3001);   
